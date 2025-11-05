12a4cbe0c7897b787d97489f71e25434d016dd0e
Viktor Nikander <vikvil91@gmail.com>
2025-11-05 19:58:45 +0100

    add AccountTest

--- test/AccountTest.java
+++ test/AccountTest.java
@@ -3,16 +3,24 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 class AccountTest {
+    Customer customerTest = new Customer("Test", "123");
+    Employee employeeTest = new Employee("Test", "123", 35000, false);
+    CheckingAccount checkingAccountTest = new CheckingAccount(10000, 3, customerTest, employeeTest);
 
     @Test
     void withdrawal() {
+        //TODO add test if needed
     }
 
     @Test
-    void deniedWithdrawal() {
+    void deniedWithdrawalTest() {
+        String expected = "Withdrawal denied due to insufficient funds";
+        String actual = checkingAccountTest.deniedWithdrawal();
+        assertEquals(expected, actual);
     }
 
     @Test
     void deposit() {
+        //TODO add test if needed
     }
 }
