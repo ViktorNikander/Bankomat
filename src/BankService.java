6279b8b08bd5bd97bc6942acc29c54d8ca216b4c
Niklas Högblom <nhogblom@gmail.com>
2025-11-06 21:20:46 +0100

    serialise & deserialise klar~

--- src/BankService.java
+++ src/BankService.java
@@ -1,13 +1,29 @@
+import java.io.*;
+
 public class BankService {
     private String pathToOurSerialisedData = "resources/data.ser";
 
 
-    public void serialize(Bank b){
-        ;
+    public void serialize(Bank b) {
+        try (ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream(pathToOurSerialisedData))){
+            oos.writeObject(b);
+            oos.flush();
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
-    public Bank deserialize(){
-        Bank b;
 
+    public Bank deserialize() {
+        Bank b;
+        try (ObjectInputStream ois = new ObjectInputStream(new FileInputStream(pathToOurSerialisedData))){
+            b = (Bank) ois.readObject();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        } catch (ClassNotFoundException e) {
+            throw new RuntimeException(e);
+        }
         return b;
     }
 }
