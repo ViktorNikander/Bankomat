486d28c6c63c94a70b835e4660924a2ce3381691
Niklas Högblom <nhogblom@gmail.com>
2025-11-06 21:17:09 +0100

    created bankservice, started work on deserialise&serialise~

---
+++ src/BankService.java
@@ -0,0 +1,13 @@
+public class BankService {
+    private String pathToOurSerialisedData = "resources/data.ser";
+
+
+    public void serialize(Bank b){
+        ;
+    }
+    public Bank deserialize(){
+        Bank b;
+
+        return b;
+    }
+}
