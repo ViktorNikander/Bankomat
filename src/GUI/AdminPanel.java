96e1464e583eff7c9a3936141ad997d80bcdbb88
ivbe0111 <berezkinivan94@gmail.com>
2025-11-17 11:48:27 +0100

    adminPanelCreated, buttons don't work yet

--- src/GUI/AdminPanel.java
+++ src/GUI/AdminPanel.java
@@ -3,6 +3,7 @@ package GUI;
 import Person.Employee;
 
 import javax.swing.*;
+import java.awt.*;
 
 public class AdminPanel extends JPanel {
 
@@ -11,13 +12,32 @@ public class AdminPanel extends JPanel {
     Employee loggedInEmployee;
     JLabel welcomeLabel = new JLabel("Welcome ");
 
+    JPanel buttonPanel = new JPanel();
+    JButton listAccounts = new JButton("List Accounts");
+    JButton listCustomers = new JButton("List Customers");
+    JButton createAccount = new JButton("Create Account");
+    JButton createCustomer = new JButton("Create Customer");
+
+    JButton logoutButton = new JButton("Logout");
+
+
     AdminPanel(JPanel previousPanel, Employee loggedInEmployee) {
         this.loggedInEmployee = loggedInEmployee;
 
+        setLayout(new BorderLayout());
         welcomeLabel.setText("Welcome " + loggedInEmployee.getName());
-        add(welcomeLabel);
+        welcomeLabel.setHorizontalAlignment(JLabel.CENTER);
+        add(welcomeLabel, BorderLayout.NORTH);
+
+        buttonPanel.setLayout(new GridLayout(2,2));
+        buttonPanel.add(listAccounts);
+        buttonPanel.add(listCustomers);
+        buttonPanel.add(createAccount);
+        buttonPanel.add(createCustomer);
+        add(buttonPanel, BorderLayout.CENTER);
         setVisible(true);
 
+        add(logoutButton, BorderLayout.SOUTH);
 
     }
 
